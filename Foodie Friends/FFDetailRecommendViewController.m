6785946f63bf7f03eadcf95207e93d7965591809
Nikhil Khanna <nikhilkh@gmail.com>
2014-05-10 23:38:02 -0700

    Greatly imrpove detail recommendation controller

--- Foodie Friends/FFDetailRecommendViewController.m
+++ Foodie Friends/FFDetailRecommendViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "FFDetailRecommendViewController.h"
+#import "Constants.h"
 
-@interface FFDetailRecommendViewController ()
+@interface FFDetailRecommendViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *commentBox;
 
 @end
 
@@ -26,7 +31,58 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    self.nameLabel.text = [self.place objectForKey:@"name"];
+    self.addressLabel.text = [self.place objectForKey:@"vicinity"];
+    self.commentBox.delegate = self;
+    //setting up touch off screen closing key
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                          action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+    [self retrievePhoto];
+    // Do any additional setup after loading the view.
+}
+
+- (void)retrievePhoto
+{
+    NSArray* photoArray = [self.place objectForKey:@"photos"];
+    if(photoArray == nil)
+    {
+        return;
+    }
+    NSDictionary* photoDict = [photoArray firstObject];
+    NSString* url = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/photo?photoreference=%@&sensor=true&maxheight=%@&maxwidth=%@&key=%@",
+                          [photoDict objectForKey:@"photo_reference"],
+                          [photoDict objectForKey:@"height"],
+                          [photoDict objectForKey:@"width"],
+                          kGoogleAPIKey];
+    NSURL* imageUrl = [NSURL URLWithString:url];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSData *imageData = [NSData dataWithContentsOfURL:imageUrl];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Update the UI
+            self.imageView.image = [UIImage imageWithData:imageData];
+        });
+    });
+}
+
+//text field delgate methods//
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self dismissKeyboard];
+    return YES;
+}
+
+-(void)dismissKeyboard
+{
+    [self.commentBox resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
